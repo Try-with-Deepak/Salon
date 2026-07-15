54ed34c661ad8fd251fba89d56cda7f442478468
Try-with-Deepak <deepak7033511325@gmail.com>
2026-07-15 17:14:48 +0530

    updated

---
+++ Script.js
@@ -0,0 +1,23 @@
+ // Tailwind Custom Configuration for Manchali branding
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        gold: {
+                            100: '#F4E8C1',
+                            400: '#E5C158',
+                            500: '#D4AF37', /* Primary Brand Gold */
+                            600: '#AA8B2C'
+                        },
+                        dark: {
+                            100: '#2b0707',
+                            200: '#080707',
+                            300: '#121212',
+                            400: '#0B0B0B'
+                        }
+                    }
+                }
+            }
+        }
+
+        
